ad9cfd649f4d773ae50f1eb687741378c245ee6f
DOPEXDINO <106437513+DOPEXDINO@users.noreply.github.com>
2022-12-21 08:19:26 +0530

    Create stacks.py

---
+++ stacks.py
@@ -0,0 +1,16 @@
+stack = []
+
+stack.append('h')
+stack.append('e')
+stack.append('y')
+
+print('Initial stack')
+print(stack)
+
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped:')
+print(stack)
